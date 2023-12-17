61e24acd618ffa2e08fcf32a9919f9e1ba8f05db
Eyabahri14 <eya.bahri@esprit.tn>
2023-12-17 19:05:25 +0100

    last one chart

--- src/views/TableList/ChartComponent.js
+++ src/views/TableList/ChartComponent.js
@@ -1,89 +1,68 @@
-import React, { useEffect, useRef } from "react";
-import moment from "moment";
-import CanvasJSReact from "@canvasjs/react-charts";
-
-const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { format } from 'date-fns';
 
 const ChartComponent = ({ selectedDates, selectedSensorIds }) => {
-  console.log("1", selectedSensorIds);
-  const chartRef = useRef(null);
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let yMin, yMax;
-        const promisesMap = new Map();
+        const datasets = [];
 
-        const promises = selectedSensorIds.map(async (sensorId) => {
-          const datesQuery = selectedDates.map((date) => `dates=${date}`).join("&");
+        for (const sensorId of selectedSensorIds) {
+          const datesQuery = selectedDates.map((date) => `dates=${date}`).join('&');
           const url = `http://localhost:3000/api/data/dashboard/${sensorId}?${datesQuery}`;
+          console.log('url renvoye par la requete', url);
+          const response = await fetch(url);
+          const data = await response.json();
+          const dataPoints = data[0].map((entry) => {
+            const year = parseInt(entry.Year, 10);
+            const month = parseInt(entry.Month, 10) - 1;
+            const day = parseInt(entry.Day, 10);
+            const hour = parseInt(entry.Hour, 10);
+            const minute = parseInt(entry.Minute, 10);
+          
+            // Check if the values are valid numbers
+            if (isNaN(year) || isNaN(month) || isNaN(day) || isNaN(hour) || isNaN(minute)) {
+              console.error('Invalid date:', entry);
+              return null;
+            }
+          
+            const dateValue = new Date(year, month, day, hour, minute);
+          
+            // Check if the date is valid
+            if (isNaN(dateValue.getTime())) {
+              console.error('Invalid date:', entry);
+              return null;
+            }
+          
+            const formattedDate = format(dateValue, 'dd/MM/yyyy HH:mm');
+          
+            return {
+              name: formattedDate,
+              [sensorId]: entry.valeur,
+            };
+          });
+          
 
-          const promise = fetch(url)
-            .then(response => response.json())
-            .then(data => {
-              const allDataPoints = data.flatMap((sensorData) =>
-                sensorData.map((entry) => ({
-                  x: moment(`${entry.Year}-${entry.Month}-${entry.Day} ${entry.Hour}:${entry.Minute}`, "YYYY-MM-DD HH:mm").toDate(),
-                  y: entry.valeur,
-                })).sort((a, b) => a.x - b.x)
-              );
+          const filteredDataPoints = dataPoints.filter((entry) => entry !== null);
 
-              yMin = Math.min(...allDataPoints.map((point) => point.y));
-              yMax = Math.max(...allDataPoints.map((point) => point.y));
+          datasets.push({
+            sensorId: sensorId,
+            data: filteredDataPoints,
+          });
+        }
 
-              const dataPoints = data.flatMap((sensorData) =>
-                sensorData.map((entry) => ({
-                  x: moment(`${entry.Year}-${entry.Month}-${entry.Day} ${entry.Hour}:${entry.Minute}`, "YYYY-MM-DD HH:mm").toDate(),
-                  y: entry.valeur,
-                })).sort((a, b) => a.x - b.x)
-              );
+        setChartData(datasets);
 
-              return {
-                type: "spline",
-                name: `Capteur ${sensorId}`,
-                showInLegend: true,
-                legendText: `Capteur ${sensorId}`,
-                dataPoints: dataPoints,
-                axisYType: "secondary",
-              };
-            });
-
-          promisesMap.set(sensorId, promise);
-        });
-
-        // Exclude the first sensor ID from the initial render
-        const initialRender = chartRef.current === null;
-        const startIdx = initialRender ? 1 : 0;
-        const chartsData = await Promise.all(selectedSensorIds.slice(startIdx).map(sensorId => promisesMap.get(sensorId)));
-
-        const options = {
-          animationEnabled: true,
-          title: {
-            text: `Débit des Capteurs ${selectedSensorIds.join(",")} le ${selectedDates.join(", ")} `,
-          },
-          axisX: {
-            title: "Time",
-            valueFormatString: "HH:mm",
-            type: "dateTime",
-          },
-          axisY: {
-            title: "Values",
-            minimum: yMin,
-            maximum: yMax,
-          },
-          axisYType: "primary",
-          legend: {
-            cursor: "pointer",
-            verticalAlign: "top",
-            horizontalAlign: "center",
-            dockInsidePlotArea: true,
-          },
-          data: chartsData,
-        };
-
-        chartRef.current = options;
+        // Ajoutez ces console.log pour voir les valeurs
+        console.log('AxX values:', chartData.flatMap((dataset) => dataset.data.map((point) => point.name)));
+        console.log('AxY values:', selectedSensorIds);
+        console.log('Data values:', chartData.flatMap((dataset) => dataset.data.map((point) => ({ name: point.name, ...point }))));
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
       }
     };
 
@@ -93,7 +72,35 @@ const ChartComponent = ({ selectedDates, selectedSensorIds }) => {
   return (
     <div>
       <h2>Dashboard</h2>
-      {chartRef.current && <CanvasJSChart options={chartRef.current} />}
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          width={500}
+          height={300}
+          data={chartData.flatMap((dataset) => dataset.data)}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" tickFormatter={(tick) => moment(tick).format('DD/MM/YYYY HH:mm')} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+
+          {selectedSensorIds.map((sensorId) => (
+            <Line
+              key={sensorId}
+              type="monotone"
+              dataKey={sensorId}
+              stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`} // Random color
+              activeDot={{ r: 8 }}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
