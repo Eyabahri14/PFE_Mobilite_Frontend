50239c523a3d6686a47690926cfc87be99c220ad
Eyabahri14 <eya.bahri@esprit.tn>
2024-01-04 14:35:58 +0100

    consommation maps

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,111 +1,5 @@
-// import React, { useEffect } from "react";
-// import "leaflet/dist/leaflet.css";
-// import L from "leaflet";
-// import "leaflet-routing-machine";
-// import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-// import "leaflet.markercluster/dist/MarkerCluster.css";
-// import "leaflet.markercluster/dist/MarkerCluster.Default.css";
-// import "leaflet.markercluster";
-// import "leaflet-control-geocoder/dist/Control.Geocoder.css";
-// import "leaflet-control-geocoder/dist/Control.Geocoder.js";
-// import "./Maps.css";
-// import Counters from "../../data/counters.json";
-// import Mesures from "../../data/mesure.json";
-
-
-
-// const DefaultIcon = L.icon({
-//   iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
-//   iconSize: [38, 38],
-//   iconAnchor: [10, 41],
-//   popupAnchor: [2, -40],
-// });
-
-// L.Marker.prototype.options.icon = DefaultIcon;
-
-
-
-// function calculateColor(value, maxValue) {
-//   const green = Math.min(255, Math.floor((1 - value / maxValue) * 255));
-//   const red = Math.min(255, Math.floor((value / maxValue) * 255));
-//   return `rgb(${red}, ${green}, 0)`;
-// }
-
-// const Maps = () => {
-//   useEffect(() => {
-//     const map = L.map("map").setView([47.312666, 5.041106], 13);
-
-//     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//       attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`,
-//     }).addTo(map);
-
-//     const markers = L.markerClusterGroup({
-//       iconCreateFunction: function (cluster) {
-//         const markersInCluster = cluster.getAllChildMarkers();
-//         let sum = 0;
-//         markersInCluster.forEach((marker) => {
-//           const counterId = marker.options.counterId;
-//           const mesure = Mesures.find((m) => m.id_capteur === counterId);
-//           if (mesure) {
-//             sum += mesure.valeur;
-//           }
-//         });
-
-//         const backgroundColor = calculateColor(sum, 1000); // 1000 est la valeur maximale (à adapter à vos données)
-   
-//         console.log(backgroundColor)
-
-//         const clusterIcon = L.divIcon({
-//           html: `${sum} nb/v/h </div>`,
-//           iconSize: [36, 36],
-//           iconAnchor: [18, 18],
-//           className: "custom-cluster-icon",
-//         });
-        
-
-//         return clusterIcon;
-//       },
-//     });
-
-//     Counters.forEach((counterData) => {
-//       const marker = L.marker([
-//         counterData.controller_lat,
-//         counterData.controller_long,
-//       ]);
-    
-//       marker.options.counterId = counterData.id;
-    
-//       const mesure = Mesures.find((m) => m.id_capteur === counterData.id);
-    
-//       if (mesure) {
-//         marker.bindPopup(`<b>${counterData.measuring_station}</b><br>Valeur : ${mesure.valeur} nb/v/h`);
-//       } else {
-//         marker.bindPopup(`<b>${counterData.measuring_station}</b><br>Pas de mesure disponible`);
-//       }
-    
-//       markers.addLayer(marker);
-//     });
-    
-
-//     map.addLayer(markers);
-
-//     L.Routing.control({
-//       waypoints: [
-//         L.latLng(47.312666, 5.041106),
-//         L.latLng(47.3266818, 5.053012893),
-//       ],
-//       geocoder: L.Control.Geocoder.nominatim(),
-//       fitSelectedRoutes: true,
-//     }).addTo(map);
-//   }, []);
-
-//   return <div id="map" className="map-container" />;
-// };
-
-// export default Maps;
-
 import React, { useEffect, useState } from "react";
-import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
@@ -115,9 +9,7 @@ import "leaflet.markercluster";
 import "leaflet-control-geocoder/dist/Control.Geocoder.css";
 import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import "./Maps.css";
-import Counters from "../../data/counters.json";
-import Mesures from "../../data/mesure.json";
-import "leaflet.heat"; // Import Heatmap Library
+import "leaflet.heat";
 
 const DefaultIcon = L.icon({
   iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
@@ -129,20 +21,34 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 function calculateColor(value, minValue, maxValue) {
-  // Calculate the color based on the value within the range
   const normalizedValue = (value - minValue) / (maxValue - minValue);
   const blue = Math.min(255, Math.floor(255 * (1 - normalizedValue)));
   const red = Math.min(255, Math.floor(255 * normalizedValue));
   return `rgb(${red}, 0, ${blue})`;
 }
+
 const Maps = () => {
+  const [mesures, setMesures] = useState([]);
+  const [capteurs, setCapteurs] = useState([]);
+
   useEffect(() => {
-    if (!Mesures || !Counters.length) return;
+    fetch('http://localhost:3000/api/data/mesure')
+      .then(response => response.json())
+      .then(data => setMesures(data));
+
+    fetch('http://localhost:3000/api/data/capteur')
+      .then(response => response.json())
+      .then(data => setCapteurs(data));
+  }, []);
+
+  useEffect(() => {
+    if (!mesures.length || !capteurs.length) return;
 
     const map = L.map("map").setView([47.312666, 5.041106], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`,
     }).addTo(map);
+
     const markers = L.markerClusterGroup({
       iconCreateFunction: function (cluster) {
         const markersInCluster = cluster.getAllChildMarkers();
@@ -152,7 +58,7 @@ const Maps = () => {
 
         markersInCluster.forEach((marker) => {
           const counterId = marker.options.counterId;
-          const mesure = Mesures.find((m) => m.id_capteur === counterId);
+          const mesure = mesures.find((m) => m.id_capteur === counterId);
           if (mesure) {
             sum += mesure.valeur;
             minValue = Math.min(minValue, mesure.valeur);
@@ -160,51 +66,31 @@ const Maps = () => {
           }
         });
 
-        const clusterIcon = L.divIcon({
+        return L.divIcon({
           html: `${sum} nb/v/h`,
           iconSize: [36, 36],
           iconAnchor: [18, 18],
           className: "custom-cluster-icon",
-          // Set the background color based on the values
-          style: `background-color: ${calculateColor(
-            sum,
-            minValue,
-            maxValue
-          )};`,
+          style: `background-color: ${calculateColor(sum, minValue, maxValue)};`,
         });
-        return clusterIcon;
       },
     });
 
-    const clusterHeatData = []; // Data for cluster heatmap
+    const clusterHeatData = [];
 
-    Counters.forEach((counterData) => {
-      const marker = L.marker([
-        counterData.controller_lat,
-        counterData.controller_long,
-      ]);
-      marker.options.counterId = counterData.id;
-      const mesure = Mesures.find((m) => m.id_capteur === counterData.id);
+    capteurs.forEach((capteur) => {
+      const marker = L.marker([capteur.Capteur_lat, capteur.Capteur_long]);
+      marker.options.counterId = capteur.id_capteur;
+      const mesure = mesures.find((m) => m.id_capteur === capteur.id_capteur);
       if (mesure) {
-        marker.bindPopup(
-          `<b>${counterData.measuring_station}</b><br>Valeur : ${mesure.valeur} nb/v/h`
-        );
-
-        // Add the data for heatmap
-        clusterHeatData.push([
-          counterData.controller_lat,
-          counterData.controller_long,
-          mesure.valeur,
-        ]);
+        marker.bindPopup(`<b>${capteur.Station_point_de_depart}</b><br>Valeur : ${mesure.valeur} nb/v/h`);
+        clusterHeatData.push([capteur.Capteur_lat, capteur.Capteur_long, mesure.valeur]);
       } else {
-        marker.bindPopup(
-          `<b>${counterData.measuring_station}</b><br>Pas de mesure disponible`
-        );
+        marker.bindPopup(`<b>${capteur.Station_point_de_depart}</b><br>Pas de mesure disponible`);
       }
       markers.addLayer(marker);
     });
 
-    // Create a heatmap layer for clusters
     L.heatLayer(clusterHeatData, { radius: 25 }).addTo(markers);
     map.addLayer(markers);
 
@@ -217,7 +103,7 @@ const Maps = () => {
       fitSelectedRoutes: true,
     }).addTo(map);
 
-  }, [Mesures]);
+  }, [mesures, capteurs]);
 
   return <div id="map" style={{ height: "1000px" }}></div>;
 };
